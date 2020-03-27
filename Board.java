24cac1f5ecc3034f3b9ff34ecfd02c2ef4674e60
dsuh02 <62726295+dsuh02@users.noreply.github.com>
2020-03-27 01:10:53 -0700

    Add files via upload

--- Board.java
+++ Board.java
@@ -1,25 +1,35 @@
-import java.awt.Dimension;
-import java.awt.Graphics;
-import javax.swing.JPanel;
-import java.awt.Image;
-import java.awt.image.BufferedImage;
-import javax.swing.ImageIcon;
-
+import java.awt.*; 
+import java.awt.event.*; 
+import javax.swing.*; 
 public class Board extends JPanel{
+    int start = 2;
     //board
     private Coord[][] coords = new Coord[16][30];
-    //bombs
-    ImageIcon bombI = new ImageIcon("bomb.png");
-    private Image bomb = bombI.getImage();    
     //tiles
     ImageIcon tileI = new ImageIcon("tile.png");
     private Image tile = tileI.getImage();  
+    //tiles
+    ImageIcon openedI = new ImageIcon("opened.png");
+    private Image opened = openedI.getImage();
+    //bombs
+    ImageIcon bombI = new ImageIcon("bomb.png");
+    private Image bomb = bombI.getImage();
     //flagged
     ImageIcon flagI = new ImageIcon("flag.png");
     private Image flag = flagI.getImage();  
     public Board() {
         
         initBoard();
+        addMouseListener(new MouseAdapter(){
+            @Override
+                public void mouseClicked(MouseEvent e) {
+                    int c =(e.getX())/30;
+                    int r =(e.getY())/30;
+                    coords[r][c].setState(coords[r][c].getImage());
+                    System.out.println(r+", "+c);
+                    repaint();
+                }
+        });
         setVisible(true);
     }
     private void initBoard(){
@@ -34,10 +44,10 @@ public class Board extends JPanel{
             for(int i=0; i<30; i++){
                 double r = Math.random();
                 if(r<.2){
-                    coords[j][i] = new Coord(j,i,bomb,0);
+                    coords[j][i] = new Coord(j,i,3,0);
                 }
                 else{
-                    coords[j][i] = new Coord(j,i,tile,0);
+                    coords[j][i] = new Coord(j,i,1,0);
                 }
                 count++;
             }
@@ -47,11 +57,21 @@ public class Board extends JPanel{
     
     @Override
     public void paintComponent(Graphics g){
-        for(int j=0; j<16; j++){
-            for(int i=0; i<30; i++){
-                Image image = coords[j][i].getImage();
-                g.drawImage(image,i*30,j*30,null);
+            for(int r=0; r<16; r++){
+                for(int c=0; c<30; c++){
+                    if(coords[r][c].matchesState()==false){
+                        Image image = tile;
+                        g.drawImage(image,c*30,r*30,null);
+                    }
+                    else
+                    {
+                        Image image;
+                        if(coords[r][c].getImage()==1)  image=opened;
+                        else if(coords[r][c].getImage()==2)  image=flag;
+                        else image=bomb;
+                        g.drawImage(image,c*30,r*30,null);
+                    }
+                }
             }
-        }
     }
 }
--- Coord.java
+++ Coord.java
@@ -1,16 +1,21 @@
 import java.awt.Image;
 
 public class Coord{
-    private int x,y;
-    private Image image;
-    private int state;
-    public Coord(int x, int y, Image i, int s){
+    private int x,y,i,state; // 0-covered 1-opened 2-flagged 3-exploded
+    public Coord(int x, int y, int i, int s){
         this.x=x;
         this.y=y;
-        this.image=i;
+        this.i=i;
         this.state = s;
     }
     public int getDown(){ return x;}    
     public int getRight(){ return y;}
-    public Image getImage(){ return image;}
+    public int getImage(){ return i;}
+    public int getState(){ return state;}
+    public void setState(int s){ state=s;}
+    public boolean matchesState(){
+        if(state==i)
+            return true;
+        return false;
+    }
 }
